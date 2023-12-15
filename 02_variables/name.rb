fc94756ef4f304645745a02d538fe925901d026e
Raymond McDonald <contact@wreckingballpublishing.com>
2023-12-15 11:08:52 -0600

    Update name.rb

--- 02_variables/name.rb
+++ 02_variables/name.rb
@@ -1,5 +1,8 @@
-print "Type in your name: "
-user_name = gets.chomp
+print "Type in your first name: "
+first_name = gets.chomp
+print "Type in your last name: "
+last_name = gets.chomp
+user_name = first_name + " " + last_name
 puts "Hey #{user_name}!"
 
 10.times { puts user_name }
