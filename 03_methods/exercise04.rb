792051df433c21ad386d5d1a8f3f4b0dfb40ce42
Raymond McDonald <contact@wreckingballpublishing.com>
2023-12-16 07:41:09 -0600

    Complete Methods exercise04

---
+++ 03_methods/exercise04.rb
@@ -0,0 +1,11 @@
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+# My guess before running this code is that it will print nothing. It will return "Yippeee!!!!" though
+# My guess was almost correct. I didn't think about line 3 hard enough, but it explicitly means it
+#   returns nothing.
