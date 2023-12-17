de6930b8bb986892032e6c1b618fb8ec17c9bb9f
Raymond McDonald <contact@wreckingballpublishing.com>
2023-12-16 20:59:19 -0600

    Complete Flow Control exercise04

---
+++ 04_flow_control/exercise04.rb
@@ -0,0 +1,31 @@
+# Snippet 1
+'4' == 4 ? puts("TRUE") : puts("FALSE")
+
+# I believe this code will output "FALSE"
+
+# Snippet 2
+x = 2
+if ((x * 3) / 2) == (4 + 4 - x - 3)
+  puts "Did you get it right?"
+else
+  puts "Did you?"
+end
+
+# I believe this code will output "Did you get it right?"
+
+# Snippet 3
+y = 9
+x = 10
+if (x + 1) <= (y)
+  puts "Alright."
+elsif (x + 1) >= (y)
+  puts "Alright now!"
+elsif (y + 1) == x
+  puts "ALRIGHT NOW!"
+else
+  puts "Alrighty!"
+end
+
+# I believe this code will output "Alright now!"
+
+# I was correct on all counts.
