8499738051e7b079a8d3e3f6312a40c8d8984f01
Raymond McDonald <contact@wreckingballpublishing.com>
2023-12-18 11:15:10 -0600

    Complete exercise10.rb

---
+++ 09_exercises/exercise10.rb
@@ -0,0 +1,7 @@
+# Hash values can be arrays. I believe hash keys can be arrays, too.
+hash_array_value = { one: [1, "one"], two: [2, "two"]}
+hash_array_keys = { [1] => "one", [2] => "two"}
+# See?
+
+# And you can, indeed, have an array of hashes
+array_of_hashes = [{one: 1, two: 2}, {three: 3, four: 4}]
