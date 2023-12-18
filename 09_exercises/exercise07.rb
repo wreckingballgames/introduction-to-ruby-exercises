eec79a75033b104aad4289fd8e69c4eb6773eab4
Raymond McDonald <contact@wreckingballpublishing.com>
2023-12-18 10:59:00 -0600

    Complete exercise07.rb

---
+++ 09_exercises/exercise07.rb
@@ -0,0 +1,3 @@
+# The major difference between an array and a hash is that an array is a list of any number of objects while
+#   a hash is a list of any number of key:value pairs
+#   Arrays are indexed by numbers in order, while hashes are indexed by their keys
