1a6fe7d336db4e907ade906b423945cc4939e58e
Raymond McDonald <contact@wreckingballpublishing.com>
2023-12-18 10:43:37 -0600

    Complete More Stuff exercise03

---
+++ 08_more_stuff/exercise03.rb
@@ -0,0 +1,5 @@
+# Exception handling is a way of writing code that anticipates exceptions, or errors, and provides alternative
+#   code to execute in case of an error. It can be used to handle errors generally or catch specific errors,
+#   and even use selective flow of execution based on what kind of error it catches.
+#   This way, you can write code that runs into errors (for example, those based on wrong user input) and still
+#   have the code complete its entire execution, even if the output is malformed as a result.
